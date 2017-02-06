abd7dbdcd1ee0d0876e5864f9c3aded314afe45d
Fedor Arbuzov <FPArbuzov@edu.hse.ru>
2017-02-06 23:57:42 +0300

    seminar1

---
+++ seminar1/task1/task1.kt
@@ -0,0 +1,9 @@
+package seminar1.task1
+
+/**
+ * Created by fedor on 05.02.17.
+ */
+
+fun main(args: Array<String>){
+    println("Hello World!")
+}
---
+++ seminar1/task2/task2.kt
@@ -0,0 +1,5 @@
+package seminar1.task2
+
+/**
+ * Created by fedor on 05.02.17.
+ */
